70cb3fda01baf8293145b6eea0e5ff1db66f2da3
menneck <zeeeee.peeeeetryyy@gmail.com>
2025-01-04 23:51:06 -0300

    feat: Changing AppLink to receive an optional Icon as Chield and fixing routes of AppLink in header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,14 @@
-import { HomeIcon } from 'lucide-react';
-import { Link } from 'react-router';
+import { Bitcoin, CircleDollarSign } from 'lucide-react';
 import AppLink from '../link/link';
 import { ModeToggle } from '../theme-toggle/theme-toggle';
-import { buttonVariants } from '../ui/button';
 import UserIconMenu from '../user-icon-menu/user-icon-menu';
 
 export default function Header() {
     return (
         <nav className="flex w-full min-h-16 border-2 bg-background mt-5 rounded-3xl items-center justify-between px-4 mb-5">
             <div className="flex gap-6">
-                <Link to={'/'} className={buttonVariants({ variant: 'ghost' })}>
-                    <HomeIcon />
-                </Link>
-                <AppLink to={'/financial'} label="Financeiro" />
-                <AppLink to={'/crypto'} label="Crypto" />
+                <AppLink to={'/'} label="Financeiro" icon={<CircleDollarSign />} />
+                <AppLink to={'/crypto'} label="Crypto" icon={<Bitcoin />} />
             </div>
             <div className="flex gap-4 items-center">
                 <ModeToggle />
--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,15 +1,14 @@
+import { ReactNode } from 'react';
 import { Link, useLocation } from 'react-router';
 import { buttonVariants } from '../ui/button';
 
-export default function AppLink({ to, label }: { to: string; label: string }) {
+export default function AppLink({ to, label, icon }: { to: string; label: string; icon?: ReactNode }) {
     const location = useLocation();
     const isActive = to == location.pathname;
 
     return (
-        <Link
-            className={`${buttonVariants({ variant: 'ghost' })} ${isActive ? 'bg-accent text-accent-foreground' : ''}`}
-            to={to}
-        >
+        <Link className={`${buttonVariants({ variant: 'ghost' })} ${isActive ? 'bg-accent text-accent-foreground' : ''}`} to={to}>
+            {icon && icon}
             {label}
         </Link>
     );
