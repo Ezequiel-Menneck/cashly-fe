153953297deec4cdc0a1a1512ed62301b7d33e85
menneck <zeeeee.peeeeetryyy@gmail.com>
2025-01-05 17:59:10 -0300

    feat(transactions): Transaction card for financial page

--- src/pages/financial/financial.tsx
+++ src/pages/financial/financial.tsx
@@ -1,4 +1,6 @@
+import { BaseSalaryChart } from '@/components/base-salary-chart/base-salary-chart';
 import FirstTimeDialog from '@/components/first-time-dialog/first-time-dialog';
+import TransactionCard from '@/components/transaction-card/transaction-card';
 import TransactionsByCategoryChart from '@/components/transactions-by-category-chart/transactions-by-category-chart';
 import { TransactionsByDateChart } from '@/components/transactions-by-date-chart/transactions-by-date-chart';
 import { useEffect, useState } from 'react';
@@ -21,9 +23,12 @@ export default function Financial() {
     return (
         <>
             <div className="flex gap-4 h-[300px]">
+                <BaseSalaryChart />
                 <TransactionsByCategoryChart />
                 <TransactionsByDateChart />
             </div>
+
+            <TransactionCard />
 
             <FirstTimeDialog open={showFirstTimeDialog} onClose={handleCloseForm} />
         </>
